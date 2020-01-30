43267b7499fac218997f7190039fd84020b3065d
Matteo <matteop18@gmail.com>
2020-01-30 14:49:32 +0100

    prime prove js

--- js/script.js
+++ js/script.js
@@ -2,3 +2,19 @@
 // Se è <= 5 il quadrato diventa giallo,
 // se è > di 5 il quadrato diventa verde.
 // Il numero ottenuto appare al centro del quadrato.
+
+$(document).ready(function() {
+  $('.square').click(function() {
+    $.ajax ({
+      'url' : 'https://flynn.boolean.careers/exercises/api/random/int',
+      'method' : 'GET',
+      'success' : function (data, stato) {
+        var response = data.response;
+        console.log(response);
+      },
+      'error' : function (richiesta, stato, errori) {
+        alert("C'è stato un errore" + errori);
+      }
+    });
+  });
+});
